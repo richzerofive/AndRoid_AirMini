e4a3d7f4573ba7b9e9f5a279364cebd844f8a93b
SeungJu <richzerofive@gmail.com>
2016-08-05 09:13:39 +0900

    완 다시 올림

---
+++ app/src/main/java/com/abc/app/airbnbcityapp/BookActivity.java
@@ -0,0 +1,47 @@
+package com.abc.app.airbnbcityapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class BookActivity extends Activity  implements View.OnClickListener{
+    TextView et_check_in,et_check_out,et_count,et_address;
+    Button bt_booking,bt_reset;
+    CityService service;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book);
+        service = new CityServiceImpl(this.getApplicationContext());
+        et_check_in = (TextView) findViewById(R.id.et_check_in);
+        et_check_out = (TextView) findViewById(R.id.et_check_out);
+        et_count = (TextView) findViewById(R.id.et_count);
+        et_address = (TextView) findViewById(R.id.et_address);
+        bt_booking = (Button) findViewById(R.id.bt_booking);
+        bt_reset = (Button) findViewById(R.id.bt_reset);
+        bt_booking.setOnClickListener(this);
+        bt_reset.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bt_booking:
+                CityBean bean = new CityBean();
+                bean.setCheckIn(et_check_in.getText().toString());
+                bean.setCheckOut(et_check_out.getText().toString());
+                bean.setCount(Integer.parseInt(et_count.getText().toString()));
+                bean.setAddress(et_address.getText().toString());
+                service.regist(bean);
+                startActivity(new Intent(this,BookListActivity.class));
+                break;
+            case R.id.bt_reset:
+                startActivity(new Intent(this,HomeActivity.class));
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/abc/app/airbnbcityapp/CustomAdapter.java
@@ -0,0 +1,45 @@
+package com.abc.app.airbnbcityapp;
+
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * Created by hb2003 on 2016-08-04.
+ */
+public class CustomAdapter extends PagerAdapter{
+    LayoutInflater inflater;
+
+    public CustomAdapter(LayoutInflater inflater) {
+        this.inflater=inflater;
+    }
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return 10;
+    }
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View view=null;
+        view= inflater.inflate(R.layout.viewpager_guideview, null);
+        ImageView img= (ImageView)view.findViewById(R.id.img_viewpager_guideimage);
+
+        img.setImageResource(R.drawable.guide_01+position);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View)object);
+    }
+    @Override
+    public boolean isViewFromObject(View v, Object obj) {
+        // TODO Auto-generated method stub
+        return v==obj;
+    }
+
+}
+
